0f2743895737ba31ce623fbd3ccba09a24d3257a
Kirill Shevchenko <79111353+9Sheva@users.noreply.github.com>
2022-07-29 14:04:07 +0300

    Add files via upload
    Информационный бот для компании, с кнопками и инлайн ссылками внутри кнопок

--- main.py
+++ main.py
@@ -1,54 +1,47 @@
-import random
 import telebot
 from telebot import types
-from telebot.types import InlineKeyboardMarkup
-from datetime import datetime, timedelta
-from datetime import time
-import pytz  # $ pip install pytz
-import tzlocal  # $ pip install tzlocal
-
-bot = telebot.TeleBot('5475765480:AAFr_N5Bj8ZAJQn1Po-8oHOuaw3sdjT1Ik8')
-
-
+token='5405357107:AAEu1oBK9fSfUv5XCEnaOEl2g7lQ0w_L7Mw'
+bot=telebot.TeleBot(token)
+#@bot.message_handler(commands=['start'])
+#def start_message(message):
+#    bot.send_message(message.chat.id,'Привет, тебе нужно нажать сюда, чтобы увидеть дополнительную информацию /button.')
 @bot.message_handler(commands=['start'])
-def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name}! Я информационный бот компании Red Machine Games. Наша команда состоит из профессионалов, имеющих за своими плечами большой опыт создания игр.\n\nИнформация, которая может быть тебе полезна:\n\n'f'Наши проекты можно просмотреть на сайте, нажав -> /site. \n\nИнформацию об активных вакансиях можно просмотреть, нажав -> /job. А если ты не нашел вакансию по своему направлению, то всегда можешь написать нашей HR Юлии в телеграме, нажав -> /hr. \n\nМожешь узнать, где находится наш офис, нажав -> /adress.\n\nА если у тебя есть для нас крутое предложение, то незамедлительно пиши на почту! mail@redmachinegames.com </b>'
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Активные вакансии")
+    item2 = types.KeyboardButton('Наши проекты')
+    item3=types.KeyboardButton('Наш офис')
+    item4=types.KeyboardButton('Сотрудничество')
+    markup.add(item1, item2, item3, item4)
+    bot.send_message(message.chat.id,'Привет! Я информационный бот компании Red Machine Games! Использую кнопки, расположенные в меню чата, чтобы найти нужную информацию:)',reply_markup=markup)
 
-    bot.send_message(message.chat.id, mess, parse_mode='html')
+@bot.message_handler(content_types=['text'], func=lambda message: message.text =='Активные вакансии')
+def func(message):
+    markup = types.InlineKeyboardMarkup()
+    url_btn = types.InlineKeyboardButton(url='https://hh.ru/employer/1343803', text='RMG HeadHunter')
+    markup.add(url_btn)
+    bot.send_message(message.chat.id, 'Перейди по ссылке', reply_markup=markup)
+
+@bot.message_handler(content_types=['text'], func=lambda message: message.text =='Наши проекты')
+def func1(message):
+    markup = types.InlineKeyboardMarkup()
+    url_btn = types.InlineKeyboardButton(url='https://www.redmachinegames.com/', text='RedMachineGames')
+    markup.add(url_btn)
+    bot.send_message(message.chat.id, 'Мы создали множество успешных игр, часть из которых ты найдешь нанашем сайте', reply_markup=markup)
 
 
-@bot.message_handler()
-def get_user_text(message):
-    if message.text == 'Hello':
-        bot.send_message(message.chat.id, 'И тебе привет!', parse_mode='html')
-    elif message.text == "/adress":
-        bot.send_message(message.chat.id, 'Москва, ул.Кожевническая 7 строение 1. Около 5-ти минут от метро "Павелецкая".', parse_mode='html')
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=='Наши проекты':
+        bot.send_message(message.chat.id,'Мы создали множество успешных игр, часть из которых можно найти на нашем сайте https://www.redmachinegames.com/')
+    elif message.text=='Наш офис':
+        bot.send_message(message.chat.id,'Мы находимся по адресу: г.Москва, ул.Кожевническая, 7/1. Недалеко от метро "Павелецкая. Всегда рады гостям!".',  parse_mode='html')
         photo = open(f'1.jpg', 'rb')
         bot.send_photo(message.chat.id, photo)
-    elif message.text == '/site':
-        website(message)
-    elif message.text == '/job':
-        websitea(message)
-    elif message.text == '/hr':
-        bot.send_message(message.chat.id, '@juliesperanskaya', parse_mode='html')
-    else:
-        bot.send_message(message.chat.id,
-                         'Пока что я так не умею, но ты можешь воспользоваться списком команд из приветственного сообщения! Если оно пропало, то нажим /start.',
-                         parse_mode='html')
+    elif message.text=='Сотрудничество':
+        bot.send_message(message.chat.id,'Если у тебя есть крутые идеи и предложения-не стесняйся, пиши на аншу почту! mail@redmachinegames.com')
 
 
-@bot.message_handler(commands=['site'])
-def website(message):
-    markup: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Red Machine Games', url="https://redmachinegames.com/"))
-    bot.send_message(message.chat.id, 'Наши проекты', reply_markup=markup)
-
-
-@bot.message_handler(command=['job'])
-def websitea(message):
-    markup: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('RMG HeadHunter', url="https://hh.ru/employer/1343803"))
-    bot.send_message(message.chat.id, 'Активные вакансии', reply_markup=markup)
 
 
 bot.polling(none_stop=True)
